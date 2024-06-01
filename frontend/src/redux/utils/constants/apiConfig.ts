358837f91fe4c7cfc310ae2a40fbca6798afb7d5
Pritam Debnath <103424346+PriDebnath@users.noreply.github.com>
2024-06-01 20:24:24 +0530

    Update apiConfig.ts

--- frontend/src/redux/utils/constants/apiConfig.ts
+++ frontend/src/redux/utils/constants/apiConfig.ts
@@ -1,3 +1,2 @@
-export const API_DOMAIN: string = "http://localhost:8000";
-//export const API_DOMAIN: string = "https://quotes-keeper.onrender.com";
-// export const API_DOMAIN : string =  "https://quotes-keeper-backend.cyclic.app"
+//export const API_DOMAIN: string = "http://localhost:8000";
+export const API_DOMAIN: string = "https://agspert-assignment.onrender.com";
